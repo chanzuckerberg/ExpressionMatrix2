85ba8ff6a0cf5ba526195166069f9f015ac20471
Paolo Carnevali <paoloczi@users.noreply.github.com>
2017-08-03 12:37:36 -0700

    Add the tests directory to the release package.

--- scripts/makeRelease.py
+++ scripts/makeRelease.py
@@ -16,16 +16,18 @@ def directoryExists(name):
 # Make sure we are in the right directory.
 message = 'The src directory is missing. This command should be invoked from the top level directory of the ExpressionMatrix2 directory.'
 currentDirectory = os.getcwd()
-if not directoryExists('src') or not directoryExists('doc') or not directoryExists('Release'):
+if not directoryExists('src') or not directoryExists('doc') or not directoryExists('Release') or not directoryExists('tests'):
     raise Exception(message)
 
 # Make sure the shared library exists.    
 if not os.path.exists('Release/ExpressionMatrix2.so'):
     raise Exception('Release/ExpressionMatrix2.so not found')
 
-# Strip the shared library, then add it to the arechive. 
+# Create the tar file with the doc and tests directories.
+os.system('tar --create --file Release.tar doc tests')
+
+# Strip the shared library, then add it to the archive at the top level.
 # Stripping it reduces its size by a large factor.    
-os.system('tar --create --file Release.tar doc')
 os.chdir('Release')  
 os.system('strip ExpressionMatrix2.so')
 os.system('tar --append --file ../Release.tar ExpressionMatrix2.so')
