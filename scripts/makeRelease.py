9dffca473748de283889522ded991d58702f608e
Paolo Carnevali <paoloczi@users.noreply.github.com>
2017-08-03 11:24:12 -0700

    Script to create a tar file for a release.

---
+++ scripts/makeRelease.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+import os
+
+"""
+This scripts creates a tar file containing a release package for the current, existing build.
+It should be invoked fom the top level directory of the ExpressionMatrix2 tree
+(that is, the directory containing src, doc, etc.). It will create a file named
+Release.tar in the same directory.
+"""
+
+# Function to check if a directory exists.
+def directoryExists(name):
+    return os.path.exists(name) and os.path.isdir(name)
+
+# Make sure we are in the right directory.
+message = 'The src directory is missing. This command should be invoked from the top level directory of the ExpressionMatrix2 directory.'
+currentDirectory = os.getcwd()
+if not directoryExists('src') or not directoryExists('doc') or not directoryExists('Release'):
+    raise Exception(message)
+
+# Make sure the shared library exists.    
+if not os.path.exists('Release/ExpressionMatrix2.so'):
+    raise Exception('Release/ExpressionMatrix2.so not found')
+
+# Strip the shared library, then add it to the arechive. 
+# Stripping it reduces its size by a large factor.    
+os.system('tar --create --file Release.tar doc')
+os.chdir('Release')  
+os.system('strip ExpressionMatrix2.so')
+os.system('tar --append --file ../Release.tar ExpressionMatrix2.so')
+
+
