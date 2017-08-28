90b619b72decee2b33a672b586c35b19927875dd
Paolo Carnevali <paoloczi@users.noreply.github.com>
2017-08-28 11:56:10 -0700

    Add an Eclipse configuration for build on centos7 (needs work). Remove the prebuilt libraries.

---
+++ Release-ubuntu16-python3/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lboost_python-py35 -lboost_filesystem -lboost_system -lpython3.5m -lboost_regex -lhdf5_cpp -lhdf5_serial
+
---
+++ Release-ubuntu16-python3/src/subdir.mk
@@ -0,0 +1,93 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/CellSets.cpp \
+../src/CellSimilarityGraph.cpp \
+../src/ClusterGraph.cpp \
+../src/ExpressionMatrix.cpp \
+../src/ExpressionMatrixBioHub.cpp \
+../src/ExpressionMatrixFindSimilarPairs.cpp \
+../src/ExpressionMatrixHdf5.cpp \
+../src/ExpressionMatrixHttpServer.cpp \
+../src/ExpressionMatrixHttpServerCells.cpp \
+../src/ExpressionMatrixHttpServerGenes.cpp \
+../src/ExpressionMatrixHttpServerGraphs.cpp \
+../src/ExpressionMatrixLsh.cpp \
+../src/ExpressionMatrixSubset.cpp \
+../src/GeneSet.cpp \
+../src/HttpServer.cpp \
+../src/MemoryMappedStringTable.cpp \
+../src/MurmurHash2.cpp \
+../src/PythonModule.cpp \
+../src/SimilarPairs.cpp \
+../src/color.cpp \
+../src/hdf5.cpp \
+../src/regressionCoefficient.cpp \
+../src/tokenize.cpp \
+../src/touchMemory.cpp 
+
+OBJS += \
+./src/CellSets.o \
+./src/CellSimilarityGraph.o \
+./src/ClusterGraph.o \
+./src/ExpressionMatrix.o \
+./src/ExpressionMatrixBioHub.o \
+./src/ExpressionMatrixFindSimilarPairs.o \
+./src/ExpressionMatrixHdf5.o \
+./src/ExpressionMatrixHttpServer.o \
+./src/ExpressionMatrixHttpServerCells.o \
+./src/ExpressionMatrixHttpServerGenes.o \
+./src/ExpressionMatrixHttpServerGraphs.o \
+./src/ExpressionMatrixLsh.o \
+./src/ExpressionMatrixSubset.o \
+./src/GeneSet.o \
+./src/HttpServer.o \
+./src/MemoryMappedStringTable.o \
+./src/MurmurHash2.o \
+./src/PythonModule.o \
+./src/SimilarPairs.o \
+./src/color.o \
+./src/hdf5.o \
+./src/regressionCoefficient.o \
+./src/tokenize.o \
+./src/touchMemory.o 
+
+CPP_DEPS += \
+./src/CellSets.d \
+./src/CellSimilarityGraph.d \
+./src/ClusterGraph.d \
+./src/ExpressionMatrix.d \
+./src/ExpressionMatrixBioHub.d \
+./src/ExpressionMatrixFindSimilarPairs.d \
+./src/ExpressionMatrixHdf5.d \
+./src/ExpressionMatrixHttpServer.d \
+./src/ExpressionMatrixHttpServerCells.d \
+./src/ExpressionMatrixHttpServerGenes.d \
+./src/ExpressionMatrixHttpServerGraphs.d \
+./src/ExpressionMatrixLsh.d \
+./src/ExpressionMatrixSubset.d \
+./src/GeneSet.d \
+./src/HttpServer.d \
+./src/MemoryMappedStringTable.d \
+./src/MurmurHash2.d \
+./src/PythonModule.d \
+./src/SimilarPairs.d \
+./src/color.d \
+./src/hdf5.d \
+./src/regressionCoefficient.d \
+./src/tokenize.d \
+./src/touchMemory.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -std=c++0x -I/usr/include/python3.5m -I/usr/include/hdf5/serial -O3 -msse4.2 -ggdb3 -Wall -Wconversion -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
