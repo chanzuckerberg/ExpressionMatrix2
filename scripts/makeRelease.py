f74d0322d1709bcf6fd2f0b2d0be617521d729a5
Paolo Carnevali <paoloczi@users.noreply.github.com>
2017-10-19 07:47:28 -0700

    Make sure the tar file extracts to a directory with a name that contains the release number.

--- scripts/makeRelease.py
+++ scripts/makeRelease.py
@@ -10,14 +10,37 @@ shared libraries for all supported platforms.
 Is should be invoked with a single argument, the name of the release 
 package to create (e. g. 0.1.0). 
 
-This script must be invoked from a directory containing the ExpressionMatrix2 tree.
+This script must be invoked from the top level directory of the 
+ExpressionMatrix2 tree (that is, from the directory containing the src, doc, 
+and tests directories).
 The following goes into the tar file:
-ExpressionMatrix2/doc
-ExpressionMatrix2/tests
-ExpressionMatrix2/platformName/ExpressionMatrix2.so (for all supported platforms).
+doc
+tests
+platformName/ExpressionMatrix2.so (for all supported platforms).
 The shared libraries are assumed to be up to date. This script
-does not attempt to rebuild them. Remember to strip each of them
-before creating the release package!
+does not attempt to rebuild them. 
+
+
+
+PROCEDURE TO CREATE A NEW RELEASE
+
+- Commit and push all pending code.
+- For each of the supported platforms:
+  * Get the latest code.
+  * Build ExpressionMatrix2.so.
+  * Strip ExpressionMatrix2.so.
+  * Check that all the tests work.
+  * Copy ExpressionMatrix2.so to the ExpressionMatrix2 tree where makeRelease.py will run.
+- Run makeRelease.py passing the release number (e. g. 0.1.0) as the argument.
+  This creates the tar file for the release.
+- Verify that the size of the tar file is reasonable.
+- Verify that the tar file extracts correctly to a directory ExpressionMatrix2-x.y.z,
+  where x.y.z is the release name.
+- In GitHub:
+  * Create the new release. 
+  * Upload the tar file.
+  * Write some release notes.
+- Remove the local copy of the tar file.
 
 """
 
@@ -46,30 +69,28 @@ releaseName = sys.argv[1]
 
 # Make sure we are in the right directory.
 if (
-    not directoryExists('ExpressionMatrix2') or 
-    not directoryExists('ExpressionMatrix2/src') or 
-    not directoryExists('ExpressionMatrix2/doc') or 
-    not directoryExists('ExpressionMatrix2/tests')):
-    raise Exception('This command should be invoked from the directory containing the ExpressionMatrix2 directory.')
+    not directoryExists('src') or 
+    not directoryExists('doc') or 
+    not directoryExists('tests')):
+    raise Exception('This command should be invoked from the top level of the ExpressionMatrix2 tree.')
 
 
 
 # Create the list of files and directories to add to the tar file.
-files = ['ExpressionMatrix2/doc', 'ExpressionMatrix2/tests']
+files = ['doc', 'tests']
 platformNames = [
     'Release-ubuntu16-python3',
     'Release-centos7-python2',
 ]
 for platformName in platformNames:
-    platformDirectory = 'ExpressionMatrix2/' + platformName
 
     # Check that the directory for this platform exists.
-    if not directoryExists(platformDirectory):
-        raise Exception('The platform directory ' + platformDirectory + ' does not exist.')
+    if not directoryExists(platformName):
+        raise Exception('The platform directory ' + platformName + ' does not exist.')
 
     # Make sure the shared library exists. 
     # We assume that is is up to date and not attempt to rebuild it.
-    sharedLibraryName = platformDirectory + '/ExpressionMatrix2.so'   
+    sharedLibraryName = platformName + '/ExpressionMatrix2.so'   
     if not regularFileExists(sharedLibraryName):
         raise Exception(sharedLibraryName + ' not found')
 
@@ -79,7 +100,8 @@ for platformName in platformNames:
 
 # Create the tar file.
 tarFileName = 'ExpressionMatrix2-' + releaseName + '.tar'
-os.system('tar --create --bzip2 --file ' + tarFileName + ' ' + ' '.join(files))
+transformOption = "--transform 's,^,ExpressionMatrix2-" + releaseName + "/,'"
+os.system(' '.join(['tar --create --bzip2 --file', tarFileName, transformOption] + files))
 
 
 
