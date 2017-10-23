db65749ab48e5fc7e75392f285bb806c51bb1dce
Paolo Carnevali <paoloczi@users.noreply.github.com>
2017-10-23 07:58:56 -0700

    Fix centos7 build.

--- Release-centos7-python2/objects.mk
+++ Release-centos7-python2/objects.mk
@@ -4,5 +4,5 @@
 
 USER_OBJS :=
 
-LIBS := -lboost_python-py35 -lboost_filesystem -lboost_system -lboost_chrono -lpython3.5m -lboost_regex -lhdf5_cpp -lhdf5_serial
+LIBS := -lboost_python-mt -lboost_filesystem -lboost_system -lboost_chrono -lpython2.7 -lboost_regex -lhdf5_hl_cpp -lhdf5_hl
 
--- Release-centos7-python2/src/subdir.mk
+++ Release-centos7-python2/src/subdir.mk
@@ -4,25 +4,21 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
-../src/Aws.cpp \
 ../src/CellGraph.cpp \
 ../src/CellSets.cpp \
 ../src/ClusterGraph.cpp \
 ../src/ExpressionMatrix.cpp \
 ../src/ExpressionMatrixBioHub.cpp \
-../src/ExpressionMatrixCells.cpp \
 ../src/ExpressionMatrixFindSimilarPairs.cpp \
 ../src/ExpressionMatrixHdf5.cpp \
 ../src/ExpressionMatrixHttpServer.cpp \
 ../src/ExpressionMatrixHttpServerCells.cpp \
-../src/ExpressionMatrixHttpServerClusterGraphs.cpp \
 ../src/ExpressionMatrixHttpServerGenes.cpp \
 ../src/ExpressionMatrixHttpServerGraphs.cpp \
 ../src/ExpressionMatrixLsh.cpp \
 ../src/ExpressionMatrixSubset.cpp \
 ../src/GeneSet.cpp \
 ../src/HttpServer.cpp \
-../src/Lsh.cpp \
 ../src/MemoryMappedStringTable.cpp \
 ../src/MurmurHash2.cpp \
 ../src/PythonModule.cpp \
@@ -34,25 +30,21 @@ CPP_SRCS += \
 ../src/touchMemory.cpp 
 
 OBJS += \
-./src/Aws.o \
 ./src/CellGraph.o \
 ./src/CellSets.o \
 ./src/ClusterGraph.o \
 ./src/ExpressionMatrix.o \
 ./src/ExpressionMatrixBioHub.o \
-./src/ExpressionMatrixCells.o \
 ./src/ExpressionMatrixFindSimilarPairs.o \
 ./src/ExpressionMatrixHdf5.o \
 ./src/ExpressionMatrixHttpServer.o \
 ./src/ExpressionMatrixHttpServerCells.o \
-./src/ExpressionMatrixHttpServerClusterGraphs.o \
 ./src/ExpressionMatrixHttpServerGenes.o \
 ./src/ExpressionMatrixHttpServerGraphs.o \
 ./src/ExpressionMatrixLsh.o \
 ./src/ExpressionMatrixSubset.o \
 ./src/GeneSet.o \
 ./src/HttpServer.o \
-./src/Lsh.o \
 ./src/MemoryMappedStringTable.o \
 ./src/MurmurHash2.o \
 ./src/PythonModule.o \
@@ -64,25 +56,21 @@ OBJS += \
 ./src/touchMemory.o 
 
 CPP_DEPS += \
-./src/Aws.d \
 ./src/CellGraph.d \
 ./src/CellSets.d \
 ./src/ClusterGraph.d \
 ./src/ExpressionMatrix.d \
 ./src/ExpressionMatrixBioHub.d \
-./src/ExpressionMatrixCells.d \
 ./src/ExpressionMatrixFindSimilarPairs.d \
 ./src/ExpressionMatrixHdf5.d \
 ./src/ExpressionMatrixHttpServer.d \
 ./src/ExpressionMatrixHttpServerCells.d \
-./src/ExpressionMatrixHttpServerClusterGraphs.d \
 ./src/ExpressionMatrixHttpServerGenes.d \
 ./src/ExpressionMatrixHttpServerGraphs.d \
 ./src/ExpressionMatrixLsh.d \
 ./src/ExpressionMatrixSubset.d \
 ./src/GeneSet.d \
 ./src/HttpServer.d \
-./src/Lsh.d \
 ./src/MemoryMappedStringTable.d \
 ./src/MurmurHash2.d \
 ./src/PythonModule.d \
@@ -98,7 +86,7 @@ CPP_DEPS += \
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -std=c++0x -I/usr/include/python2.7 -O3 -msse4.2 -ggdb3 -c -fmessage-length=0 -Wno-unused-result -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -std=c++0x -I/usr/include/python2.7 -O3 -msse4.2 -ggdb3 -c -fmessage-length=0 -Wno-unused-result -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
