eb29b509925f3010e4a8e8a6eb1f9dc2c399e043
Paolo Carnevali <paoloczi@users.noreply.github.com>
2017-10-24 10:54:57 -0700

    Fast computation of cell LSH signatures.

--- src/Lsh.cpp
+++ src/Lsh.cpp
@@ -5,6 +5,7 @@
 using namespace ChanZuckerberg;
 using namespace ExpressionMatrix2;
 
+#include <boost/chrono.hpp>
 #include <boost/random/mersenne_twister.hpp>
 #include <boost/random/normal_distribution.hpp>
 #include <boost/random/variate_generator.hpp>
@@ -84,26 +85,99 @@ void Lsh::computeCellLshSignatures(const ExpressionMatrixSubset& expressionMatri
     CZI_ASSERT(!lshVectors.empty());
     const size_t lshCount = lshVectors.front().size();
 
+    // Get the number of genes and cells in the gene set and cell set we are using.
+    const auto geneCount = expressionMatrixSubset.geneCount();
+    const auto cellCount = expressionMatrixSubset.cellCount();
+    CZI_ASSERT(lshVectors.size() == geneCount);
+
+    // Compute the sum of the components of each lsh vector.
+    // It is needed below to compute the contribution of the
+    // expression counts that are zero.
+    vector<double> lshVectorsSums(lshCount, 0.);
+    for(GeneId localGeneId=0; localGeneId!=geneCount; localGeneId++) {
+        const auto& v = lshVectors[localGeneId]; // Components of all LSH vectors for this gene.
+        CZI_ASSERT(v.size() == lshCount);
+        for(size_t i=0; i<lshCount; i++) {
+            lshVectorsSums[i] += v[i];
+        }
+    }
+
     // Initialize the cell signatures.
     cout << timestamp << "Initializing cell LSH signatures." << endl;
-    const auto cellCount = expressionMatrixSubset.cellCount();
     signatures.resize(cellCount, BitSet(lshCount));
 
     // Vector to contain, for a single cell, the scalar products of the shifted
     // expression vector for the cell with all of the LSH vectors.
     vector<double> scalarProducts(lshCount);
 
+
+
     // Loop over all the cells in the cell set we are using.
     // The CellId is local to the cell set we are using.
     cout << timestamp << "Computation of cell LSH signatures begins." << endl;
-    for(CellId cellId=0; cellId<cellCount; cellId++) {
-        if((cellId % 1000) == 0) {
-            cout << timestamp << "Working on cell " << cellId << " of " << cellCount << endl;
+    size_t nonZeroExpressionCount = 0;
+    const auto t0 = boost::chrono::steady_clock::now();
+    for(CellId localCellId=0; localCellId<cellCount; localCellId++) {
+        if((localCellId % 1000) == 0) {
+            cout << timestamp << "Working on cell " << localCellId << " of " << cellCount << endl;
+        }
+
+        // Compute the mean of the expression vector for this cell.
+        const ExpressionMatrixSubset::Sum& sum = expressionMatrixSubset.sums[localCellId];
+        const double mean = sum.sum1 / double(geneCount);
+
+        // If U is one of the LSH vectors, we need to compute the scalar product
+        // s = X*U, where X is the cell expression vector, shifted to zero mean:
+        // X = x - mean,
+        // mean = sum(x)/geneCount (computed above).
+        // We get:
+        // s = (x-mean)*U = x*U - mean*U = x*U - mean*sum(U)
+        // We computed sum(U) above and stored it in lshVectorSums for
+        // each of the LSH vectors.
+        // Initialize the scalar products for this cell
+        // with all of the LSH vectors to -mean*sum(U).
+        for(size_t i=0; i<lshCount; i++) {
+            scalarProducts[i] = -mean * lshVectorsSums[i];
+        }
+
+        // Now add to each scalar product the x*U portion.
+        // For performance, the loop over genes is outside,
+        // which gives better memory locality.
+        // Add the contributions of the non-zero expression counts for this cell.
+        for(const auto& p : expressionMatrixSubset.cellExpressionCounts[localCellId]) {
+            const GeneId localGeneId = p.first;
+            const double count = double(p.second);
+            ++nonZeroExpressionCount;
+
+            // Add the contribution of this gene to the scalar products.
+            const auto& v = lshVectors[localGeneId];
+            CZI_ASSERT(v.size() == lshCount);
+            for(size_t i=0; i<lshCount; i++) {
+                scalarProducts[i] += count * v[i];
+            }
+        }
+
+        // Set to 1 the signature bits corresponding to positive scalar products.
+        auto& cellSignature = signatures[localCellId];
+        for(size_t i=0; i<lshCount; i++) {
+            if(scalarProducts[i]>0.) {
+                cellSignature.set(i);
+            }
         }
 
     }
+    const auto t1 = boost::chrono::steady_clock::now();
     cout << timestamp << "Computation of cell LSH signatures ends." << endl;
+    cout << "Processed " << nonZeroExpressionCount << " non-zero expression counts for ";
+    cout << geneCount << " genes and " << cellCount << " cells." << endl;
+    cout << "Average expression matrix sparsity is " <<
+        double(nonZeroExpressionCount) / (double(geneCount) * double(cellCount)) << endl;
+    const double t01 = 1.e-9 * double((boost::chrono::duration_cast<boost::chrono::nanoseconds>(t1 - t0)).count());
+    cout << "Computation of LSH cell signatures took " << t01 << "s." << endl;
+    cout << "    Seconds per cell " << t01 / cellCount << endl;
+    cout << "    Seconds per non-zero expression matrix entry " << t01/double(nonZeroExpressionCount) << endl;
+    cout << "    Seconds per inner loop iteration " << t01 / (double(nonZeroExpressionCount) * double(lshCount)) << endl;
+    cout << "    Gflop/s " << 2. * 1e-9 * double(nonZeroExpressionCount) * double(lshCount) / t01 << endl;
 
-    CZI_ASSERT(0);
 }
 
