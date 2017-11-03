2e29aa2ff9ae65da0ce7ac5f8f9e2683d43c9d2d
Paolo Carnevali <paoloczi@users.noreply.github.com>
2017-11-03 11:06:08 -0700

    Add forgotten new files.

---
+++ tests/CaseStudy1/runServer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+from ExpressionMatrix2 import *
+
+# Access our existing expression matrix.
+e = ExpressionMatrix(directoryName = 'data')
+
+# Run the server.
+e.explore()
+
+
