c33a9493b19d0d1d4b1aa97da4f26b0448d98b84
Paolo Carnevali <paoloczi@users.noreply.github.com>
2017-11-07 09:59:49 -0800

    Remove dependency on Boost.Python library.

--- Release-centos7-python2/objects.mk
+++ Release-centos7-python2/objects.mk
@@ -4,5 +4,5 @@
 
 USER_OBJS :=
 
-LIBS := -lboost_python-mt -lboost_filesystem -lboost_system -lboost_chrono -lpython2.7 -lboost_regex -lhdf5_hl_cpp -lhdf5_hl
+LIBS := -lboost_filesystem -lboost_system -lboost_chrono -lpython2.7 -lboost_regex -lhdf5_hl_cpp -lhdf5_hl
 
