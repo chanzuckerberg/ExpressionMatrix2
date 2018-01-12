96d1ca300a4d2c8f824394eb9e7d5d0f0f32496a
Paolo Carnevali <paoloczi@users.noreply.github.com>
2018-01-12 09:49:23 -0800

    Add some scripts to simplify creating a new release.

---
+++ scripts/makeAll.py
@@ -0,0 +1,128 @@
+#!/usr/bin/python3
+
+"""
+
+This script does a clone from GitHub to create 
+a brand new code tree at the specified path
+and builds all of the supported configurations for the
+system we are running on.
+
+Run with two arguments:
+- The system we are running on.
+  Currently, this can only be ubuntu16 or centos7.
+- The path to the new code tree to be created.
+It must not exist.
+
+This scripts assumes that all packages required by the build
+are installed.
+
+"""
+
+# Import what we need.
+import os
+import sys
+
+# Check the number of arguments.
+if len(sys.argv) != 3:
+    raise Exception(
+    'Invoke with two arguments:\n' 
+    '- The system to build for (ubuntu16 or centos7).\n'
+    '- The path to the code tree to be created.\n')
+
+
+# The platforms their options.
+ubuntu16Platforms = {
+    'Release-ubuntu16-python3' :
+    [
+    ],
+    'Release-ubuntu16-python2' :
+    [
+    '-DPYTHON_INCLUDE_PATH=/usr/include/python2.7', 
+    '-DPYBIND11_INCLUDE_PATH=/usr/local/include/python2.7',
+    ],
+    'Release-ubuntu16-nohdf5-python3' :
+    [
+    '-DBUILD_WITH_HDF5=OFF',
+    ],
+    'Release-ubuntu16-nohdf5-python2' :
+    [
+    '-DPYTHON_INCLUDE_PATH=/usr/include/python2.7', 
+    '-DPYBIND11_INCLUDE_PATH=/usr/local/include/python2.7',
+    '-DBUILD_WITH_HDF5=OFF',
+    ],
+    }
+centos7Platforms = {
+    'Release-centos7-python3' :
+    [
+    '-DHDF5_INCLUDE_PATH=/usr/include',
+    '-DHDF5_LIBRARIES="hdf5_cpp hdf5"',
+    ],
+    'Release-centos7-python2' :
+    [
+    '-DPYTHON_INCLUDE_PATH=/usr/include/python2.7', 
+    '-DPYBIND11_INCLUDE_PATH=/usr/lib/python2.7/site-packages',
+    '-DHDF5_INCLUDE_PATH=/usr/include',
+    '-DHDF5_LIBRARIES="hdf5_cpp hdf5"',
+    ],
+    'Release-centos7-nohdf5-python3' :
+    [
+    '-DBUILD_WITH_HDF5=OFF',
+    ],
+    'Release-centos7-nohdf5-python2' :
+    [
+    '-DPYTHON_INCLUDE_PATH=/usr/include/python2.7', 
+    '-DPYBIND11_INCLUDE_PATH=/usr/lib/python2.7/site-packages',
+    '-DBUILD_WITH_HDF5=OFF',
+    ],
+    }
+
+allPlatforms = {
+    'ubuntu16': ubuntu16Platforms,
+    'centos7':  centos7Platforms,
+    }
+
+# Get the platforms we are going to build
+systemName = sys.argv[1]
+if not systemName in allPlatforms.keys():
+    raise Exception('The first argument must be one of ' + ' '.join(allPlatforms.keys()))
+platforms = allPlatforms[systemName]
+    
+
+# Get the path where the new code tree should be created.
+path = sys.argv[2]
+
+# Check that it does not already exists.
+if os.path.exists(path):
+    raise Exception(path + ' already exists.')    
+    
+# Create it and cd to it.
+os.mkdir(path)
+os.chdir(path)
+
+# Clone the GitHub repository.
+os.system('git clone https://github.com/chanzuckerberg/ExpressionMatrix2')
+os.chdir('ExpressionMatrix2')
+
+# Create the build directory and cd to it.
+os.mkdir('build')
+os.chdir('build')
+
+# Build all platforms
+for platform in platforms:
+    print('Building ' + platform)
+    options = platforms[platform]
+    os.mkdir(platform)
+    os.chdir(platform)
+    returnCode = os.system('cmake ../../src ' + ' '.join(options))
+    if returnCode:
+        raise Exception('cmake error for ' + platform)
+    returnCode = os.system('make -j all')
+    if returnCode:
+        raise Exception('Build error for ' + platform)
+    os.system('strip ExpressionMatrix2.so')
+    os.chdir('..')
+
+
+
+
+
---
+++ scripts/runToyTests.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+
+"""
+
+This script runs the toy tests on all the platforms
+build by the makeAll script appropriate for the
+system we are running on.
+
+"""
+
+# Import what we need.
+import glob
+import os
+import shutil
+import sys
+
+
+# Get the path of the code tree to test.
+if len(sys.argv) != 2:
+    raise Exception('Invoke with one argument, the path to the code tree to be tested.')
+path = sys.argv[1]
+
+# Check that it exists.
+if not os.path.exists(path):
+    raise Exception(path + ' does not exist.')    
+    
+# cd to the build directory.
+os.chdir(path)
+os.chdir('ExpressionMatrix2/build')
+
+
+
+# Loop over all platforms we find.
+for platform in glob.glob('*'):
+    print('Testing ' + platform)
+    
+    # Find the name of the python command for this platform
+    if platform.find('python3') >= 0:
+        pythonCommand = 'python3'
+    else:
+        pythonCommand = 'python'
+    print(pythonCommand)
+    
+    # Go to the build directory for this platform.
+    os.chdir(platform)
+    
+    # Point to the ExpressionMatrix2.so here.
+    os.environ['PYTHONPATH'] = os.getcwd()
+    
+    # Copy the toy tests here.
+    os.system('cp -rp ../../tests/ToyTest* .') 
+    
+    # Loop over the toy tests.
+    for test in glob.glob('ToyTest*'):
+        print('Testing %s on %s.' % (test, platform))
+        os.chdir(test)
+        print('Test directory is ' + os.getcwd())
+        if os.path.exists('data'):
+            shutil.rmtree('data')
+        returnCode = os.system(pythonCommand + ' run.py')
+        print(returnCode)
+        if returnCode:
+            raise Exception('Test %s failed on %s.' % (test, platform))
+        os.chdir('..')
+
+    # Go back to the build directory.
+    os.chdir('..')
+    
+
+
+
+
