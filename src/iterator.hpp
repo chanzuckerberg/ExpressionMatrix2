7b9a135258bf9790972cb35f7ccad24dc5a09f13
Paolo Carnevali <paoloczi@users.noreply.github.com>
2018-03-23 07:20:21 -0700

    Fix typo in include guard.

--- src/iterator.hpp
+++ src/iterator.hpp
@@ -1,5 +1,5 @@
 #ifndef CZI_EXPRESSION_MATRIX2_ITERATOR_HPP
-#define CZI_EXPRESSION_MATRIX1_ITERATOR_HPP
+#define CZI_EXPRESSION_MATRIX2_ITERATOR_HPP
 
 // Add to the ExpressionMatrix2 namespace some names defined in namespace std.
 
